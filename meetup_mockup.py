9588f4d5b7022595819627865d9f22b4118acb7d
Pat Viafore <patviafore@gmail.com>
2017-02-04 11:22:18 -0600

    Adding a few tests and example code

--- meetup_mockup.py
+++ meetup_mockup.py
@@ -1,7 +1,20 @@
 class Meetup(object):
 
-    def __init__(self, organizer):
-        self.organizer = organizer
+    def __init__(self, organizer, email=None):
+        self.attendees = [organizer]
+        self.emails = []
+        if email:
+            self.emails.append(email)
 
     def get_attendees(self):
-        return [self.organizer]
+        return self.attendees
+
+    def add_attendee(self, attendee, email=None):
+        if email:
+            self.emails.append(email)
+        self.attendees.append(attendee)
+
+    def send_message_to_all_attendees(self, message, emailer):
+        for email_address in self.emails:
+            emailer.email(email_address, message)
+        
--- test_meetup_mockup.py
+++ test_meetup_mockup.py
@@ -1,11 +1,35 @@
 import unittest
 from meetup_mockup import Meetup
 
+
+class MockEmailer(object):
+    def __init__(self):
+        self.messages_received = {}
+
+    def email(self, to, message):
+        if to in self.messages_received:
+            assert False
+        self.messages_received[to] = message
+        
+
 class TestMeetupMockup(unittest.TestCase):
 
     def test_new_meetup_has_only_organizer_as_attendee(self):
         meetup = Meetup("Pat Viafore")
         assert meetup.get_attendees() == ["Pat Viafore"]
 
-        meetup2 = Meetup("Patrick Viafore")
-        assert meetup2.get_attendees() == ["Patrick Viafore"]
+        meetup2 = Meetup("Zane Viafore")
+        assert meetup2.get_attendees() == ["Zane Viafore"]
+
+    def test_can_add_attendees(self):
+        meetup = Meetup("Pat Viafore")
+        meetup.add_attendee("Zane Viafore")
+        assert ["Pat Viafore", "Zane Viafore"] == sorted(meetup.get_attendees())
+
+    def test_can_send_out_message(self):
+        meetup = Meetup("Pat Viafore", "patviafore@gmail.com")
+        meetup.add_attendee("Zane Viafore", "icantevenreadyet@gmail.com")
+        emailer = MockEmailer()
+        meetup.send_message_to_all_attendees("Hooray, Python", emailer)
+        assert emailer.messages_received["patviafore@gmail.com"] == "Hooray, Python"
+        assert emailer.messages_received["icantevenreadyet@gmail.com"] == "Hooray, Python"
